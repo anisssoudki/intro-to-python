1d4e89cd4c4bfa96c87efcfc4dc7390b2fdb661d
Aniss <59289660+anisssoudki@users.noreply.github.com>
2021-02-07 17:56:31 +0200

    Update imported_module_example.py
    added how to see all the methods we write in our modules and print them using dir(module_name)

--- imported_module_example.py
+++ imported_module_example.py
@@ -5,3 +5,14 @@ print(my_module.pi)
 print(my_module.name)
 
 print(my_module.somevariable)
+
+
+
+print(my_module.pi)
+
+import random   
+ 
+print('Random number from 0 to 1 :', random.random())
+print(dir(random))
+print('there are several methods that are inherited new methods will be towards the end')
+print(dir(my_module))
