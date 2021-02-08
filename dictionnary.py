aee475b30dd487d37a6d9ce6f583db30832ec78f
Aniss <59289660+anisssoudki@users.noreply.github.com>
2021-02-08 13:55:12 +0200

    Create dictionnary.py

---
+++ dictionnary.py
@@ -0,0 +1,13 @@
+
+dictionnary = {
+    'name': 'aniss',
+    'phone': 6023944514,
+    'employed': True
+    }
+
+print(dictionnary["name"])
+print(dictionnary["phone"])
+for thing in dictionnary:
+    print(thing)
+    print(dictionnary[thing])
+
